84ab300f6025cf05bce7eba45584fe6f71089a1d
blacktea5317 <blacktea5317@gmail.com>
2024-04-22 15:36:04 +0800

    add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <>
-      <div>first-react</div>
-    </>
-  )
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
 
-export default App
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div>Here is home page!</div>
+    </>
+  );
+}
