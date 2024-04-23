945135fedbb2458f3477bb6c29af13ec47680b2a
blacktea5317 <blacktea5317@gmail.com>
2024-04-23 15:50:56 +0800

    update NotFound

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,24 @@
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Img from '../assets/internet_404_page_not_found.png';
+
 export default function NotFound() {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
-    </>
+    <Container className="not-foung-page" maxWidth="xl">
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={Img} />
+        <Typography variant="h5" sx={{ m: 5 }}>
+          Sorry, the page you are looking for could not be found.
+        </Typography>
+      </Box>
+    </Container>
   );
 }
