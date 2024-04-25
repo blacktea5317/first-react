04d879906f075dab2b34f73cc2b3c26a64316425
blacktea5317 <blacktea5317@gmail.com>
2024-04-25 15:08:56 +0800

    create bus page

--- src/pages/Bus.tsx
+++ src/pages/Bus.tsx
@@ -1,7 +1,98 @@
-export default function Bus() {
+import { grey } from '@mui/material/colors';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
+//測試data
+import top100Films from '../data/top100Films';
+
+//import { useState, useEffect } from 'react';
+
+function Bus() {
   return (
-    <>
-      <div>Here is Bus page!</div>
-    </>
+    <Container className="kao-bus" maxWidth="xl">
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          backgroundColor: grey[50],
+        }}
+      >
+        <Typography variant="h3" sx={{ m: 5 }}>
+          🚌🚌🚌
+        </Typography>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="請輸入路線名稱" />
+          )}
+        />
+        <Button className="add-btn" variant="contained" sx={{ m: 3 }}>
+          Search
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Button
+          className="go-btn"
+          variant="contained"
+          color="success"
+          sx={{ m: 3, width: '50%' }}
+        >
+          去程
+        </Button>
+        <Button
+          className="back-btn"
+          variant="outlined"
+          color="success"
+          sx={{ m: 3, width: '50%' }}
+        >
+          返程
+        </Button>
+      </Box>
+      <Timeline>
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            09:30 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>第1站</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            10:00 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+          </TimelineSeparator>
+          <TimelineContent>第2站</TimelineContent>
+        </TimelineItem>
+      </Timeline>
+    </Container>
   );
 }
+
+export default Bus;
